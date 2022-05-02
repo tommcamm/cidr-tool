39211a940c02d91eb2353d9ee1a6c444c97d8a83
Tommaso Cammelli <tomm.camm@protonmail.ch>
2022-05-02 23:06:29 +0200

    fixed syntax issues

--- src/utils/sql_connector.rs
+++ src/utils/sql_connector.rs
@@ -1,10 +1,9 @@
 // sql related stuff will be here
 
-use std::ops::Add;
-use mysql::{Pool, PooledConn};
+use mysql::{OptsBuilder, Pool, PooledConn};
 use mysql::prelude::Queryable;
 
-pub fn write_to_mysql(mut conn :PooledConn, table :Str) -> Result<Ok, Err(E)> {
+pub fn write_to_mysql(mut conn :PooledConn, table :&str) -> mysql::Result<()> {
     // create the table using a very efficient store format [only for probationary period]
     conn.query_drop(r"CREATE TABLE cidrip (
                                 ip unsigned int not null)")?;
@@ -12,11 +11,15 @@ pub fn write_to_mysql(mut conn :PooledConn, table :Str) -> Result<Ok, Err(E)> {
     Ok(())
 }
 
-pub fn mysql_create_conn(db_user :Str, db_pass :Str,
-                         db_name :Str, db_uri :Str) -> Result<Ok(T), Err(E)> {
-    let url = String::from("mysql://").add(db_user).add(":").add(db_pass)
-        .add("@").add(db_uri).add("/").add(db_name);
+pub fn mysql_create_conn(db_user :&str, db_pass :&str,
+                         db_name :&str, db_uri :&str) -> mysql::Result<PooledConn> {
 
-    let pool = Pool::new(url)?;
+    let builder = OptsBuilder::new()
+        .ip_or_hostname(Some(db_uri))
+        .db_name(Some(db_name))
+        .user(Some(db_user))
+        .pass(Some(db_pass));
+
+    let pool = Pool::new(builder)?;
     pool.get_conn()
 }
